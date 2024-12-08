678541f90f49cb65548c09f544584c01271f5054
Surya Guthikonda <suryakguthikonda@gmail.com>
2024-12-08 13:27:43 -0500

    Renamed the dataset extraction folder and refactored the code.

---
+++ dataset/extraction/constants.py
@@ -0,0 +1,12 @@
+import os
+
+DATA_BUCKET_NAME = "ai-therapist-data"
+DATA_DIR = "data"
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY', 'AIzaSyAE1ad6U3fy-Q4TyoIkzWHhWTX8FSwF9-Y')
+ASSEMBLYAI_API_KEY = os.environ.get('ASSEMBLYAI_API_KEY', '9af21f3f8465492486d796fc8825cbe5')
+PLAYLIST_IDS = [
+    "PLEJO7-F_VqlPA0GmswOTvR1xM5TCeFCfM",
+    "PLG1WzYaHQeh6JPLZcavyPLiz0ST4K406B",
+    "PLdlFfrVsmlvCcIf77MOdU3fa6uQqmtFwR",
+    "PLdlFfrVsmlvBmd0-FiSi6nRm1RNoFMt4x"
+]
